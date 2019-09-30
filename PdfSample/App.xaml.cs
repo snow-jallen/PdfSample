bc878c69f9e0717b63b62c9c55333981a20cbcea
Jonathan Allen <jallen@snow.edu>
2019-09-30 10:26:31 -0600

    Add project files.

---
+++ PdfSample/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+using CommonServiceLocator;
+using GemBox.Document;
+using Prism.Ioc;
+using Prism.Unity;
+
+namespace PdfSample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return ServiceLocator.Current.GetInstance<MainWindow>();
+        }
+
+        protected override void OnLoadCompleted(NavigationEventArgs e)
+        {
+            base.OnLoadCompleted(e);
+
+            //Initialize GemBox.Document
+            ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+
+        protected override void ConfigureServiceLocator()
+        {
+            base.ConfigureServiceLocator();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ PdfSample/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PdfSample
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+        {
+            Title = DateTime.Now.ToString();
+        }
+
+        public string Title { get; private set; }
+    }
+}
